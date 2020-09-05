f226ab6d19d5d44a3f59efdc2cf5f2650635605c
michael <mps0@protonmail.com>
2020-09-06 01:40:23 +0200

    some cleanup.

--- main.cpp
+++ main.cpp
@@ -28,30 +28,14 @@ int main(void) {
     Image image = Image(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
-    //test points
-    Vec2i p0 = Vec2i(320, 50);
-    Vec2i p1 = Vec2i(20, 400);
-    Vec2i p2 = Vec2i(620, 400);
-    Vec2i p3 = Vec2i(200, 100);
-
-    Vec2i p0a = Vec2i(320, 100);
-    Vec2i p1a = Vec2i(320, 300);
-    Vec2i p2a = Vec2i(430, 200);
-
-    Vec2i p0b = Vec2i(320, 100);
-    Vec2i p1b = Vec2i(500, 400);
-    Vec2i p2b = Vec2i(200, 100);
-
-
-
     bool run = true;
     int i = 0;
+    float fps = 0.f;
     while(run) {
         auto exec_start = std::chrono::high_resolution_clock::now();
-        
+
         i++;
-
-
+        i = i % 2000;
         float pi = 3.141592;
         float alpha = (i / 1000.f) * pi;
 
@@ -64,60 +48,46 @@ int main(void) {
         rotY.c1 = Vec4f(0.f, 1.f, 0.f, 0.f);
         rotY.c2 = Vec4f(sina, 0.f, cosa, 0.f);
         rotY.c3 = Vec4f(0.f, 0.f, 0.f, 1.f);
-            
-    Mat4f trans = Mat4f();
-    trans.c0 = Vec4f(1.f, 0.f, 0.f, 0.f);
-    trans.c1 = Vec4f(0.f, 1.f, 0.f, 0.f);
-    trans.c2 = Vec4f(0.f, 0.f, 1.f, 0.f);
-    trans.c3 = Vec4f(0.f, 0.f, 2.f, 1.f);
 
-    Mat4f trans2 = Mat4f();
-    trans2.c0 = Vec4f(1.f, 0.f, 0.f, 0.f);
-    trans2.c1 = Vec4f(0.f, 1.f, 0.f, 0.f);
-    trans2.c2 = Vec4f(0.f, 0.f, 1.f, 0.f);
-    trans2.c3 = Vec4f(0.f, 0.f, -2.f, 1.f);
+        Mat4f trans = Mat4f();
+        trans.c0 = Vec4f(1.f, 0.f, 0.f, 0.f);
+        trans.c1 = Vec4f(0.f, 1.f, 0.f, 0.f);
+        trans.c2 = Vec4f(0.f, 0.f, 1.f, 0.f);
+        trans.c3 = Vec4f(0.f, 0.f, 2.f, 1.f);
 
-
+        Mat4f trans2 = Mat4f();
+        trans2.c0 = Vec4f(1.f, 0.f, 0.f, 0.f);
+        trans2.c1 = Vec4f(0.f, 1.f, 0.f, 0.f);
+        trans2.c2 = Vec4f(0.f, 0.f, 1.f, 0.f);
+        trans2.c3 = Vec4f(0.f, 0.f, -2.f, 1.f);
 
 
 
-    Vec4f p0 = Vec4f(-0.5f, -0.5f, -2.f, 1.f);
-    Vec4f p1 = Vec4f(0.5f, -0.5f, -2.f, 1.f);
-    Vec4f p2 = Vec4f(0.0f, 0.5f, -2.f, 1.f);
-    
-
-    p0 = trans * p0;
-    p1 = trans * p1;
-    p2 = trans * p2;
+        Vec4f p0 = Vec4f(-0.5f, -0.5f, -2.f, 1.f);
+        Vec4f p1 = Vec4f(0.5f, -0.5f, -2.f, 1.f);
+        Vec4f p2 = Vec4f(0.0f, 0.5f, -2.f, 1.f);
 
 
-    p0 = rotY * p0;
-    p1 = rotY * p1;
-    p2 = rotY * p2;
+        p0 = trans * p0;
+        p1 = trans * p1;
+        p2 = trans * p2;
 
 
-    p0 = trans2 * p0;
-    p1 = trans2 * p1;
-    p2 = trans2 * p2;
+        p0 = rotY * p0;
+        p1 = rotY * p1;
+        p2 = rotY * p2;
 
 
-       printf("px0: %f, pz0: %f\n", p0.x, p0.z);
+        p0 = trans2 * p0;
+        p1 = trans2 * p1;
+        p2 = trans2 * p2;
 
-    image.clear();
 
-    pipeline(&image, p0, p1, p2);
-    //drawLines(p0, p1, &image);
-    //drawLines(p1, p2, &image);
-    //drawLines(p2, p0, &image);
-   
-    //drawLines(p0a, p1a, &image);
-    //drawLines(p1a, p2a, &image);
-    //drawLines(p2a, p0a, &image);
-    
-    //drawLines(p1b, p2b, &image);
-    //
+
+        pipeline(&image, p0, p1, p2);
 
         SDL_UpdateTexture(texture, NULL, image.pixels, SCREEN_WIDTH * sizeof(Uint32));
+        image.clear();
 
         if(SDL_PollEvent(&event) && event.type == SDL_QUIT){
             break;
@@ -126,8 +96,13 @@ int main(void) {
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
-    auto exec_end = std::chrono::high_resolution_clock::now();
-    printf("Loop time:%i ms\n", std::chrono::duration_cast<std::chrono::milliseconds>(exec_end - exec_start).count());
+        auto exec_end = std::chrono::high_resolution_clock::now();
+        auto exec_time = std::chrono::duration_cast<std::chrono::milliseconds>(exec_end - exec_start).count(); 
+        //float exec_time = std::chrono::duration_cast<std::chrono::microseconds>(exec_end - exec_start).count(); 
+        //fps = 1000000.f / exec_time;
+        //printf("FPS: %i\n", (int)fps);
+        //
+        printf("execution time: %i ms\n", exec_time);
     }
 
     SDL_DestroyTexture(texture);
